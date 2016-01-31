c2d5bb5ccda95f9cab33ea794dc1e6dbaa06a555
John Tyree <johntyree@gmail.com>
2016-01-31 12:34:33 -0600

    MAINT: refactor rendering script

--- vegfi/veg.py
+++ vegfi/veg.py
@@ -1,119 +1,132 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""<+Module Description.+>"""
+"""
+Batch script for generating pre-rendered veg.fi menu pages.
+"""
 
 from datetime import datetime
 from datetime import timedelta
-import json
 import logging
 import re
 import requests
 
 from jinja2 import Template
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8.8s [%(name)s:%(lineno)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
 
 SOURCE_API_URL = 'http://www.lounasaika.net/api/v1/menus.json'
 TEMPLATE_FILE = 'template.html'
 
+DAYS = ('maanantai', 'tiistai', 'keskiviikko', 'torstai', 'perjantai',
+        'lauantai', 'sunnuntai')
+
+# This is ordered
+FOOD_TYPES = (
+    ('potato', re.compile("perunoita \(")),
+    ('carrot', re.compile("porkkan(oit)?aa? \(|Porkkanaraaste")),
+    ('strawberry', re.compile("mansikoita \(")),
+    ('seed', re.compile("siemeniä \(")),
+    ('rice', re.compile("riisiä \(")),
+    ('zucchini', re.compile("kesäkurpitsaviipaleita \(")),
+    ('smoothie', re.compile("smoothie \(")),
+    ('soup', re.compile("keittoa? \(")),
+    ('lentils', re.compile("linssejä")),
+    ('stew', re.compile("pataa \(")),
+    ('tomato', re.compile("tomaatti \(")),
+)
+
+MEAL_TYPES = (
+    ("main", re.compile("(pataa|pihvejä|currya) \(")),
+)
+
+MEAL_CLEAN_RC = re.compile('(?:, L, M, Veg)|(?:\*, )')
+
 
 def get_weekday_name(weekday_index):
-    if weekday_index == 0:
-        return 'maanantai'
-    elif weekday_index == 1:
-        return 'tiistai'
-    elif weekday_index == 2:
-        return 'keskiviikko'
-    elif weekday_index == 3:
-        return 'torstai'
-    elif weekday_index == 4:
-        return 'perjantai'
-    elif weekday_index == 5:
-        return 'lauantai'
-    elif weekday_index == 6:
-        return 'sunnuntai'
-    else:
+    try:
+        return DAYS[weekday_index]
+    except IndexError:
         return 'VIRHE'
 
 
-def guess_food_type(food):
-
-    if re.search(".*perunoita \(", food) is not None:
-        return 'potato'
-    if re.search(".*porkkan(oit)?aa? \(|Porkkanaraaste.*", food) is not None:
-        return 'carrot'
-    # if re.search("Porkkanaraaste.*", food) is not None:
-        # return 'carrot'
-    if re.search(".*mansikoita \(", food) is not None:
-        return 'strawberry'
-    if re.search(".*siemeniä \(", food) is not None:
-        return 'seed'
-    if re.search(".*riisiä \(", food) is not None:
-        return 'rice'
-    if re.search(".*kesäkurpitsaviipaleita \(", food) is not None:
-        return 'zucchini'
-    if re.search(".*smoothie \(", food) is not None:
-        return 'smoothie'
-    if re.search(".*keittoa? \(", food) is not None:
-        return 'soup'
-    if re.search(".*linssejä.*", food) is not None:
-        return 'lentils'
-    if re.search(".*pataa \(", food) is not None:
-        return 'stew'
-    if re.search(".*tomaatti \(", food) is not None:
-        return 'tomato'
-    return 'generic'
+def guess_type(item, search_regexen, default=None):
+    """ Return the tag associated with the first matching regex or None. """
+    for tag, reg in search_regexen:
+        if reg.search(item):
+            return tag
+    return default
 
 
-def guess_meal_type(food):
+def clean_meal_string(meal):
+    return MEAL_CLEAN_RC.sub('', meal)
 
-    if re.search(".*pataa \(", food) is not None:
-        return 'main'
-    if re.search(".*pihvejä \(", food) is not None:
-        return 'main'
-    if re.search(".*currya \(", food) is not None:
-        return 'main'
+
+def retrieve_meal_data():
+    """Return a list of dict of restaurant data from the API.
+
+    Raise a requests.HTTPError on failure.
+    """
+    response = requests.get(SOURCE_API_URL)
+    response.raise_for_status()
+    return response.json()
+
+
+def acceptable_restaurant(restaurant, lang='fi'):
+    R = restaurant
+    if ('name' in R and 'meals' in R and R['meals'].get(lang) is not None):
+        return True
+    return False
+
+
+def extract_vegan_meal(meal):
+    """ Return a vegan meal tuple of (meal description, food_type) or None. """
+    if 'Veg' in meal:
+        # Add food type information
+        food_type = guess_type(meal, FOOD_TYPES, default='generic')
+        # Remove redundant information
+        meal = clean_meal_string(meal)
+        return (meal, food_type)
+    return None
 
 
 def get_menu(lang='fi', date=datetime.today()):
     """Returns dict of restaurants to filtered list of meals."""
 
-    response = requests.get(SOURCE_API_URL)
-    response_content = response.text
-    response_json = json.loads(response_content)
-    vegmeals = {}
-    logging.debug('date.weekday(): %s' % date.weekday())
-    weekday = date.weekday()
-    logging.debug('weekday: %s' % weekday)
+    update_time = datetime.now().strftime('%a %d %b %Y %H:%M')
+    logger.debug("Update time: %s" % update_time)
 
-    for restaurant in response_json:
-        name = restaurant.get('name')
-        meals = restaurant.get('meals', {})
-        if meals:
-            meals_en = meals.get(lang, [])
-            if meals_en:
-                vegmeals_local = list()
-                logging.debug('len(meals_en): %s' % len(meals_en))
-                # TODO: Collect entire week's data, not just one day.
-                try:
-                    for meal in meals_en[weekday]:
-                        if meal.find("Veg") != -1:
-                            # Add food type information
-                            food_type = guess_food_type(meal)
-                            # Remove redundant information
-                            meal = re.sub('(?:, L, M, Veg)|(?:\*, )', '', meal)
-                            vegmeals_local.append([meal, food_type])
-                except IndexError:
-                    logging.debug(
-                            'Out of range for %s.' % restaurant.get('name')
-                            )
-                if vegmeals_local:
-                    vegmeals[name] = vegmeals_local
+    weekday = date.weekday()
+    logger.debug('weekday: %s' % weekday)
     day_name = get_weekday_name(weekday)
 
-    update_time = datetime.now().strftime('%a %d %b %Y %H:%M')
-    logging.debug("Update time: %s" % update_time)
-    return vegmeals, day_name, update_time
+    vegmeals = dict()
+    restaurants = retrieve_meal_data()
+    for restaurant in restaurants:
+        if not acceptable_restaurant(restaurant, lang=lang):
+            logger.debug("Incomplete restaurant: {}".format(restaurant))
+            continue
+
+        name = restaurant['name']
+        meals = restaurant['meals']
+        meals_lang = meals[lang]
+        if len(meals_lang) < weekday - 1:
+            # Not enough days to reach this weekday... somehow
+            msg = 'Number of days :: len(meals_lang): {} {}'
+            logger.debug(msg.format(name, len(meals_lang)))
+            continue
+
+        meal_iter = (extract_vegan_meal(m) for m in meals_lang[weekday])
+        vegmeals_local = [meal for meal in meal_iter if meal is not None]
+        if vegmeals_local:
+            vegmeals[name] = vegmeals_local
+
+    sorted_meals = sorted(vegmeals.items())
+    return sorted_meals, day_name, update_time
 
 
 def get_plaintext_menu(lounas_dict):
